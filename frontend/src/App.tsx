0e94a64f96dfad9ef8fffacb71f46859eef40e74
nomadcoderkor <nomadcoder.kor@gmail.com>
2026-02-09 15:59:13 +0900

    test deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,6 +33,7 @@ function ProtectedLayout() {
       } catch {
         navigate("/login", { replace: true });
       } finally {
+        console.log("finally");
         setLoading(false);
       }
     };
